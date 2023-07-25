d60b9a0cd71e03a344e0810f1fcde7fd4d1a1273
LorinczNorbertAttila <lorincz.norbert@student.ms.sapientia.ro>
2023-07-25 14:49:24 +0300

    Added firebase, changed Camera app to Image picker

--- animateon/lib/home.dart
+++ animateon/lib/home.dart
@@ -1,4 +1,7 @@
+import 'dart:io';
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+
 
 class Home extends StatefulWidget {
   const Home({super.key});
@@ -8,13 +11,27 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+
+  var _image;
+  final imagePicker = ImagePicker();
+
+  Future getImage() async{
+   final image = await imagePicker.pickImage(
+      source: ImageSource.camera
+    );
+    if (image == null) return;
+    setState(() {
+     _image = File(image.path);
+    });
+  }
+  
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+      return Scaffold(
       backgroundColor: Colors.grey[900],
       appBar: AppBar(
         elevation: 0,
-        title: Text(
+        title: const Text(
           'Animate On',
           style: TextStyle(
             fontSize: 25,
@@ -27,10 +44,10 @@ class _HomeState extends State<Home> {
       body: Center(
         child: Column(
           children: [
-            SizedBox(
+            const SizedBox(
               height: 150,
             ),
-            Text(
+            const Text(
               'You can start your journey',
               style: TextStyle(
                 fontSize: 25,
@@ -38,7 +55,7 @@ class _HomeState extends State<Home> {
                 fontFamily: 'LilitaOne',
               ),
             ),
-            Text(
+            const Text(
               'by scanning an image:',
               style: TextStyle(
                 fontSize: 25,
@@ -46,13 +63,11 @@ class _HomeState extends State<Home> {
                 fontFamily: 'LilitaOne',
               ),
             ),
-            SizedBox(
+            const SizedBox(
               height: 150,
             ),
             FloatingActionButton.large(
-              onPressed: () {
-                Navigator.pushNamed(context, '/camera');
-              },
+              onPressed: getImage,
               backgroundColor: Colors.amber[300],
               child: Icon(Icons.camera_alt, color: Colors.grey[900], size: 55),
             ),
@@ -62,3 +77,4 @@ class _HomeState extends State<Home> {
     );
   }
 }
+
--- animateon/lib/main.dart
+++ animateon/lib/main.dart
@@ -2,7 +2,7 @@ import 'camera_app.dart';
 import 'package:flutter/material.dart';
 import 'home.dart';
 
-void main() {
+void main() async{
   runApp(MaterialApp(
     initialRoute: '/home',
     routes: {
